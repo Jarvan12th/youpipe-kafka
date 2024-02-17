4ab4cfe2528f0ff9ed3d8b58a7ec5cfae4f4df14
Jarvan Lee <jarvan-jobs@outlook.com>
2024-02-17 22:14:40 +1100

    feat: initial commit

---
+++ src/main/java/com/jarvanlee/youpipekafka/YoupipeKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.jarvanlee.youpipekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoupipeKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoupipeKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jarvanlee/youpipekafka/YoupipeKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarvanlee.youpipekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoupipeKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
